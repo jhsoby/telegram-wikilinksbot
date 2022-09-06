509df266510ca7f38bc4ea7d15c759b109f389f3
Jon Harald Søby <jhsoby@gmail.com>
2022-09-07 00:18:57 +0200

    Support links in media captions (#29)

--- wikilinksbot.py
+++ wikilinksbot.py
@@ -436,7 +436,8 @@ def findlinks(update, context):
     and entity schemas. It will however _not_ post a link when the entity is
     mentioned as part of a URL.
     """
-    linklist = re.findall(regex, update.message.text) # List all matches with the main regex
+    messagetext = update.message.caption or update.message.text
+    linklist = re.findall(regex, messagetext) # List all matches with the main regex
     fmt_linklist = [] # Formatted link list
     hide_preview = True
     for link in linklist:
@@ -703,12 +704,14 @@ def start(update, context):
 #updater.dispatcher.add_handler(echo_handler)
 
 link_handler = MessageHandler(Filters.regex(regex), findlinks)
+media_link_handler = MessageHandler(Filters.caption_regex(regex), findlinks)
 config_handler = CommandHandler(['setwiki', 'setlang', 'toggle', 'listconfig'], config)
 search_handler = CommandHandler('search', search)
 start_handler = CommandHandler(['start', 'help'], start)
 delete_handler = CommandHandler('delete', delete)
 
 updater.dispatcher.add_handler(link_handler)
+updater.dispatcher.add_handler(media_link_handler)
 updater.dispatcher.add_handler(config_handler)
 updater.dispatcher.add_handler(search_handler)
 updater.dispatcher.add_handler(start_handler)
